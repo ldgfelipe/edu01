2158976bb4ab5a6451e8c19f41c4214ea66b59fa
ldgframe <ldgfelipecarrera@gmail.com>
2020-08-14 23:12:09 -0500

    make it better

---
+++ components/Menugral/Menugral.vue
@@ -0,0 +1,3 @@
+<template src="./Menugral.html"></template>
+<style src="./Menugral.css"></style>
+<script src="./Menugral.js"></script>
---
+++ pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+            {{title}}
+        
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    data(){
+        return {
+            title:'Login reactivo'
+        }
+    }
+}
+</script>
